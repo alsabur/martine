7f58bda87001815ea47a9c46082b06b2d08c7c5a
Al Sabur <alsabur4@gmail.com>
2022-03-15 22:54:23 +0600

    rev 1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,50 @@
 $(document).ready(function(){
+
     $('#hotel-form').show();
     $('#ticket-form').hide();
     $('#place-form').hide();
 
-});
+    $('#hotel-item span').addClass('line');
 
+// click on hotel
+$('#hotel-item').click(function(){
+
+    $('#hotel-form').show();
+    $('#ticket-form').hide();
+    $('#place-form').hide();
+
+    $('#hotel-item span').addClass('line');
+    $('#ticket-item span').removeClass('line');
+    $('#place-item span').removeClass('line');
+
+    });
+
+// click on ticket
+
+    $('#ticket-item').click(function(){
+
+    $('#hotel-form').hide();
+    $('#ticket-form').show();
+    $('#place-form').hide();
+
+    $('#hotel-item span').removeClass('line');
+    $('#ticket-item span').addClass('line');
+    $('#place-item span').removeClass('line');
+
+    });
+
+    //click on place
+    $('#place-item').click(function(){
+
+        $('#hotel-form').hide();
+        $('#ticket-form').hide();
+        $('#place-form').show();
+    
+        $('#hotel-item span').removeClass('line');
+        $('#ticket-item span').removeClass('line');
+        $('#place-item span').addClass('line');
+    
+        });
+
+
+});
